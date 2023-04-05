77b46654206d0519daeb3c0a9baad2cec6ca409c
Amaury CLOT <amauryclt@gmail.com>
2023-04-05 20:11:52 +0200

    ex corrigé

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -9,6 +9,15 @@ const pokemonList = [
     },
 ];
 
+function PokemonCard() {
+    const pokemon = pokemonList[0];
+    return (
+        <figure>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    )
+}
+
 
 export default PokemonCard;
-
