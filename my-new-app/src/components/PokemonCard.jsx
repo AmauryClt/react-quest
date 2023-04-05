5e48207824188646addafef0c0e3853ffc367b86
Amaury CLOT <amauryclt@gmail.com>
2023-04-05 19:57:08 +0200

    react 03

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,12 +1,14 @@
-function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bullbizar" />
-            <figcaption>
-                Bulbasaur
-            </figcaption>
-        </figure>
-    )
-}
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 
 export default PokemonCard;
+
