b5af60c4817d0dc13274cf370d3f93d167f69a52
CLOT Amaury <amauryclt@gmail.com>
2023-04-15 10:16:44 +0200

    react08 fini

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -5,67 +5,43 @@ import PropTypes from "prop-types";
 import Navbar from './components/navbar';
 
 
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
-
-  PokemonCard.propTypes = {
-    pokemon: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      imgSrc: PropTypes.string
-    }).isRequired,
-  }
-
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-
-
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const nextPokemon = () => {
-    if (pokemonIndex < pokemonList.length - 1) {
-      setPokemonIndex(ancienIndex => ancienIndex + 1);
-    }
-  }
-
-
-  const previousPokemon = () => {
-    if (pokemonIndex > 0)
-      setPokemonIndex(ancienIndex => ancienIndex - 1);
-  }
-
-  const pokemon = pokemonList[pokemonIndex];
 
   return (
     <div>
-
-      <PokemonCard pokemon={pokemon} />
-      <Navbar next={nextPokemon} previous={previousPokemon} />
-
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <Navbar setPokemonIndex={setPokemonIndex} pokemonList={pokemonList} />
     </div>
-  );
+  )
 }
 
 export default App
